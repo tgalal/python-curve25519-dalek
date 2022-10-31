a970b31d5dc11028ce93a1413b4bc1928abb111c
Tarek Galal <tare2.galal@gmail.com>
2022-10-31 05:23:39 +0100

    Add Lizard encode and decode

--- src/ristretto.rs
+++ src/ristretto.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
+use pyo3::exceptions::PyValueError;
+use sha2::Sha256;
 use curve25519_dalek::ristretto::RistrettoPoint as _RistrettoPoint;
 use curve25519_dalek::ristretto::CompressedRistretto as _CompressedRistretto;
 use curve25519_dalek::scalar::Scalar as _Scalar;
@@ -27,6 +29,20 @@ impl RistrettoPoint {
     }
 
     #[staticmethod]
+    pub fn lizard_encode_sha256(data: &[u8]) -> RistrettoPoint {
+        let mut data16 : [u8; 16] = Default::default();
+        data16.copy_from_slice(&data[..]);
+        RistrettoPoint(_RistrettoPoint::lizard_encode::<Sha256>(&data16))
+    }
+
+    pub fn lizard_decode_sha256(&self, py: Python) -> PyResult<PyObject> {
+        match self.0.lizard_decode::<Sha256>() {
+            Some(data) => Ok(PyBytes::new(py, &data).into()),
+            None => Err(PyValueError::new_err("Decode failed"))
+        }
+    }
+
+    #[staticmethod]
     pub fn multiscalar_mul(scalars: Vec<PyRef<Scalar>>,
         points: Vec<PyRef<RistrettoPoint>>) -> RistrettoPoint
     {
@@ -48,8 +64,25 @@ impl CompressedRistretto {
     pub fn new(data: &[u8]) -> CompressedRistretto {
         CompressedRistretto(_CompressedRistretto::from_slice(data))
     }
+
     pub fn as_bytes(&self, py: Python) -> PyObject {
         PyBytes::new(py, self.0.as_bytes()).into()
+    }
+
+    pub fn to_bytes(&self, py: Python) -> PyObject {
+        PyBytes::new(py, &self.0.to_bytes()).into()
+    }
+
+    pub fn decompress(&self) -> PyResult<RistrettoPoint> {
+        let decompressed = self.0.decompress();
+        match decompressed {
+            Some(point) => Ok(RistrettoPoint(point)),
+            None => Err(PyValueError::new_err("Decompress failed"))
+        }
+    }
+    #[staticmethod]
+    pub fn from_slice(bytes: &[u8]) -> CompressedRistretto {
+        CompressedRistretto(_CompressedRistretto::from_slice(bytes))
     }
 }
 
